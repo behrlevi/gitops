1e915e525dc237c028cd428ad1f5b35ffe9a650e
behrlevi <sidolevente@aol.com>
2025-11-13 21:57:38 +0000

    fix(FE): fixed the deliberate error

--- src/frontend/src/frontend/main.py
+++ src/frontend/src/frontend/main.py
@@ -1,4 +1,4 @@
-rom flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import requests
 import logging
 from datetime import datetime
